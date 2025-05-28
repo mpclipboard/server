ec9b958d01d66c2660e68c49ba160e4a9d21f7cc
Ilya Bylich <ibylich@gmail.com>
2025-05-28 14:44:13 +0200

    write config in toml instead of json

--- server/src/config.rs
+++ server/src/config.rs
@@ -26,20 +26,20 @@ impl Config {
             port: 3000,
             token: Uuid::new_v4().to_string(),
         };
-        let json = serde_json::to_string_pretty(&config).expect("static values, can't fail");
-        println!("{}", json);
+        let toml = toml::to_string_pretty(&config).expect("static values, can't fail");
+        println!("{}", toml);
     }
 
     pub(crate) fn read() -> Result<Self> {
         let path = if cfg!(debug_assertions) {
-            "config.json"
+            "config.toml"
         } else {
-            "/etc/shared-clipboard-server/config.json"
+            "/etc/shared-clipboard-server/config.toml"
         };
 
         let content =
             std::fs::read_to_string(path).with_context(|| format!("failed to read {path}"))?;
-        let config: Self = serde_json::from_str(&content)?;
+        let config: Self = toml::from_str(&content)?;
         Ok(config)
     }
 }
