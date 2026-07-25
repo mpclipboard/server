fb1e1cde19ca614d2523807f536ff64e22af82fa
Ilya Bylich <ibylich@gmail.com>
2026-07-25 14:52:59 +0200

    rename macos/mpclipboard to macos/src

--- macos/scripts/generate-asset-contents-json.py
+++ macos/scripts/generate-asset-contents-json.py
@@ -4,7 +4,7 @@ import json
 from pathlib import Path
 
 
-ASSETS_DIR = Path("mpclipboard/Assets.xcassets")
+ASSETS_DIR = Path("src/Assets.xcassets")
 INFO = {"author": "xcode", "version": 1}
 IMAGESETS = ("green", "red", "yellow")
 APPICON_SLOTS = (
