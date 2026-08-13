b8c881cda2c0ce8ac5596e0198f706a5ca7986c8
Ilya Bylich <ibylich@gmail.com>
2026-08-13 20:07:50 +0200

    actually enable TCP keepalive

--- shared/src/tcp_keep_alive.rs
+++ shared/src/tcp_keep_alive.rs
@@ -1,10 +1,14 @@
 use crate::trace;
 use core::time::Duration;
-use rustix::net::sockopt::{set_tcp_keepcnt, set_tcp_keepidle, set_tcp_keepintvl};
+use rustix::net::sockopt::{
+    set_socket_keepalive, set_tcp_keepcnt, set_tcp_keepidle, set_tcp_keepintvl,
+};
 use std::os::fd::AsFd;
 
 pub fn enable_tcp_keep_alive(fd: &impl AsFd) -> std::io::Result<()> {
     trace!("Configuring TCP keepalive");
+
+    set_socket_keepalive(fd, true)?;
 
     // start probing after one second of idle
     set_tcp_keepidle(fd, Duration::from_secs(1))?;
